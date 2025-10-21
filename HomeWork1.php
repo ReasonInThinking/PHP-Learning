bb15c2898b06c19ff9a6ad01b71f988a1038b458
ReasonInThinking <107506673+ReasonInThinking@users.noreply.github.com>
2025-10-21 15:53:37 +0500

    Create HomeWork1.php

---
+++ HomeWork1.php
@@ -0,0 +1,24 @@
+<?php
+
+# Возраст 
+$age = 10;
+
+# Основной окрас
+$main_color = "белый";
+
+# Цвет глаз
+$eyes_color = "голубой";
+
+# Красивый - ДА!
+$beauty = true;
+
+# выводим значения 
+echo $age;
+
+echo $main_color;
+
+echo $eyes_color;
+
+echo $beauty;
+
+# результат 10белыйголубой1
